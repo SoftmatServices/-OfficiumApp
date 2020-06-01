3ec80ae266f27c9c258e986bc311d967bde94bbc
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-06-01 12:22:14 -0500

    repo Camilo
    elementos Camilo

---
+++ Camilo Ruiz/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import './App.css';
+import Logo from './Components/Logo';
+import './Components/Logo.css'; 
+import './Components/Actualizacion.css'
+import Cuenta from './Components/Cuenta';
+import Actualizacion from './Components/Actualizacion';
+
+
+
+function App() {
+  return (
+    <div className= "login-form">
+      <Actualizacion />
+    </div>
+  );
+}
+
+export default App;
