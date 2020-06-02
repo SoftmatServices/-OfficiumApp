f0f22b52174541f5662e7a123417b4f075d80d22
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-06-02 12:00:24 -0500

    Create Departamento.js

---
+++ src/Components/Departamento.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+const Departamento = ({}) => (
+	              
+    <div className="col-6 col-xs-12 col-sm-6 col-md-4 col-lg-3 mt-2 bg-white">
+        <div className="title">
+        <select>        
+          <option>Ingrese su Departamento</option>
+          <option>Atlántico</option>
+          <option>Bolívar</option>
+          <option>Nariño</option>
+          <option>Santander</option>
+          <option>Valle del Cauca</option>
+          <option>Antioquia</option>
+          <option>Atlántico</option>
+          <option>Caldas</option>
+          <option>Cesar</option>
+          <option>Córdoba</option>
+          <option>Cundinamarca</option>
+          <option>Huila</option>
+          <option>Magdalena</option>
+          <option>Meta</option>
+          <option>Norte de Santander</option>
+          <option>Quindío</option>
+          <option>Risaralda</option>
+          <option>Tolima</option>
+  
+        </select>    
+        </div>
+                 
+    </div>
+
+ 
+  
+)
+
+export default Departamento;
