d064e96fa3cf915ef178bb7ecf411f799fe681cd
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-06-18 15:53:27 -0500

    Update Login.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
-// eslint-disable-next-line no-unused-vars
 import PropTypes from 'prop-types';
-import Cuenta from "../Cuenta";
+import Cuenta from "./Cuenta";
 import './Styles.css';
 import {
   BrowserRouter as Router,
