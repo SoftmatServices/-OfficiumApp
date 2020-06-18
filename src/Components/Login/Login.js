d28d71a6f3a607b76f4993a5325af0bb16f61dd2
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-06-18 12:42:06 -0500

    test

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -12,45 +12,45 @@ class Login extends React.Component {
 
 render(){
     return(
-		<div class="d-flex justify-content-center h-100">
-			<div class="card">
-				<div class="card-header">
+		<div className="d-flex justify-content-center h-100">
+			<div className="card">
+				<div className="card-header">
 					<h3>Iniciar sesión</h3>
-					<div class="d-flex justify-content-end social_icon">
-						<span><i class="fab fa-facebook-square"></i></span>
-						<span><i class="fab fa-instagram-square"></i></span>
-						<span><i class="fab fa-twitter-square"></i></span>
+					<div className="d-flex justify-content-end social_icon">
+						<span><i className="fab fa-facebook-square"></i></span>
+						<span><i className="fab fa-instagram-square"></i></span>
+						<span><i className="fab fa-twitter-square"></i></span>
 					</div>
 				</div>
-				<div class="card-body">
+				<div className="card-body">
 					<form>
-						<div class="input-group form-group">
-							<div class="input-group-prepend">
-								<span class="input-group-text"><i class="fas fa-user"></i></span>
+						<div className="input-group form-group">
+							<div className="input-group-prepend">
+								<span className="input-group-text"><i className="fas fa-user"></i></span>
 							</div>
-							<input type="text" class="form-control" placeholder="Nombre de usuario"/>
+							<input type="text" className="form-control" placeholder="Nombre de usuario"/>
 						</div>	s
 						
-						<div class="input-group form-group">
-							<div class="input-group-prepend">
-								<span class="input-group-text"><i class="fas fa-key"></i></span>
+						<div className="input-group form-group">
+							<div className="input-group-prepend">
+								<span className="input-group-text"><i className="fas fa-key"></i></span>
 							</div>
-							<input type="password" class="form-control" placeholder="Contraseña"/>
+							<input type="password" className="form-control" placeholder="Contraseña"/>
 						</div>
-						<div class="row align-items-center remember">
+						<div className="row align-items-center remember">
 							<input type="checkbox"/>Recordarme
 						</div>
-						<div class="form-group">
-							<input type="submit" value="Ingresar" class="btn float-right login_btn"/>
+						<div className="form-group">
+							<input type="submit" value="Ingresar" className="btn float-right login_btn"/>
 						</div>
 					</form>
 				</div>
-				<div class="card-footer">
-					<div class="d-flex justify-content-center links">
+				<div className="card-footer">
+					<div className="d-flex justify-content-center links">
 						No tienes una cuenta?<Link to="/Cuenta">Registrate</Link>					
 					</div>
-					<div class="d-flex justify-content-center">
-						<a href="#">Olvidaste tu contraseña?</a>
+					<div className="d-flex justify-content-center">
+						<a href=" ">Olvidaste tu contraseña?</a>
 					</div>
 				</div>
 			</div>
