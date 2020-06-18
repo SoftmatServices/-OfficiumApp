a894907d5f7c3097ae5d094dd97491c0928d8ac1
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-06-18 15:53:19 -0500

    Update Departamento.js

--- src/Components/Departamento.js
+++ src/Components/Departamento.js
@@ -1,6 +1,7 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
 import PropTypes from 'prop-types';
-const Departamento = ({}) => (
+const Departamento = () => (
 	              
     <div className="col-6 col-xs-12 col-sm-6 col-md-4 col-lg-3 mt-2 bg-white">
         <div className="title">
