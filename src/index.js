99e482641190243115861177740b0689f79b1cb5
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-06-19 11:50:56 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import '../src/css/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 //import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App/>, document.getElementById('root'))
