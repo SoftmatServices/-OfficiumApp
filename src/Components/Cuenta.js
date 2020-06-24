78fd4cea76eedbea3099b8fa24401bdb3025ecaf
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-06-24 15:23:08 -0500

    Update Cuenta.js

--- src/Components/Cuenta.js
+++ src/Components/Cuenta.js
@@ -92,10 +92,50 @@ class Cuenta extends Component {
     return (
       <div className="d-flex justify-content-center h-100">
         <div className="card">
-          <h3>Regitra tus Datos</h3>
+          <div className="card-header">
+            <h3>Regitra tus Datos</h3>
+          </div>
+          <div className="card-body">
+            <form>
+              <div className="input-group form-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text"><i className="fas fa-user"></i></span>
+                </div>
+                <input type="text" className="form-control" placeholder="Nombres"/>
+              </div>	
+
+              <div className="input-group form-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text"><i className="far fa-user"></i></span>
+                </div>
+                <input type="text" className="form-control" placeholder="Apellidos"/>
+              </div>
+              
+
+              <div className="input-group form-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text"><i className="fas fa-at"></i></span>
+                </div>
+                <input type="password" className="form-control" placeholder="Correo"/>
+              </div>
+
+              <div className="input-group form-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text"><i className="fas fa-key"></i></span>
+                </div>
+                <input type="password" className="form-control" placeholder="Contraseña"/>
+              </div>
+            
+              <div className="form-group">
+                <input type="submit" value="Registrarme" className="btn float-right login_btn"/>
+              </div>
+            </form>
+          </div>
+          <div className="card-footer">
+            
+          </div>
         </div>
-        
-      </div>
+		  </div>
     );
   }
 }
