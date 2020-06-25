655e06488e117ab953c9a76bacf6863172adbc38
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-06-25 13:41:42 -0500

    Update Cuenta.js

--- src/Components/Cuenta.js
+++ src/Components/Cuenta.js
@@ -104,6 +104,10 @@ class Cuenta extends Component {
                 <input type="text" className="form-control" placeholder="Nombres"/>
               </div>	
 
+              <div>
+                  <Cuenta />
+              </div>
+
               <div className="input-group form-group">
                 <div className="input-group-prepend">
                   <span className="input-group-text"><i className="far fa-user"></i></span>
