5e0c2324f2522268f92bc7b729f1679e057ea48e
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-06-30 12:36:56 -0500

    Update Olvido.js

---
+++ src/Components/Olvido.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class Olvido extends React.Component {
+
+render(){
+    return(
+		<div className="d-flex justify-content-center h-100">
+            
+		</div>
+    )
+  }
+}
+
+export default Olvido;
