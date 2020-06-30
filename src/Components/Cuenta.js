83724ae71b1b62bc9aa8c798fde53098ac2dd944
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-06-30 15:20:13 -0500

    Update Cuenta.js

--- src/Components/Cuenta.js
+++ src/Components/Cuenta.js
@@ -1,12 +1,84 @@
 import React, {Component} from 'react';
+import { getDeptosAll, getMpios } from '../Services/Get';
 class Cuenta extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deptos: [],
+      mpios: [],
+      name: '',
+      email: '',
+      surname: '',
+      Mpios: '',
+      nameError: '',
+      emailError: '',
+      surnameError: '',
+      cityError: '',
+      direction: '',
+      main: '',
+      secondary: '',
+      profession: '',
+      department: ''
+    };
+  }
+
+
+  fillToSubscribeDeptos = (event) => {
+    this.setState({
+      department: event.target.value
+    });
+    this.onListMpios(this.department);
+
+    console.log(this.department)
+  }
+
+  fillToSubscribeMpios = (event) => {
+    this.setState({
+      Mpios: event.target.value
+    });
+
+  }
+
+  async componentDidMount() {
+    this.onListDeptos();
+  }
+
+  onListDeptos = async () => {
+    const resDeptos = await getDeptosAll()
+    this.setState({ deptos: resDeptos.data.deptos });
+  }
+
+  onListMpios = async (q) => {
+    const resMpios = await getMpios(q)
+    this.setState({ mpios: resMpios.data.mpios });
+  }
+
+  onCreateUser = async () => {
+    let ad = {
+      name: this.name.value,
+      description: this.refs.description.value,
+      serviceCatAdId: this.refs.serviceCatAdId.value,
+      serviceUserId: this.refs.serviceUserId.value
+
+    };
+    fetch('http://efactura.softmatservices.com/v1/createAd', {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: ad
+    }).then(r => r.json()).then(res => {
+      if (res) {
+        this.setState({ message: 'New Employee is Created Successfully' });
+      }
+    });
+  }
+
   render(){
     return(
       <div className="d-flex justify-content-center h-100">
           <div className="card">
-          <div className="card-header">
-            <h3>Regitra tus Datos</h3>
-          </div>
+            <div className="card-header">
+              <h3>Regitra tus Datos</h3>
+            </div>
           <div className="card-body">
             <form>
               <div className="input-group form-group">
