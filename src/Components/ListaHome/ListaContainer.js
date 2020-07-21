701923f462ed1fe032422e15997ef2d5b8cecfa4
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-07-21 10:10:39 -0500

    Update ListaContainer.js

--- src/Components/ListaHome/ListaContainer.js
+++ src/Components/ListaHome/ListaContainer.js
@@ -53,6 +53,39 @@ class ListaContainer extends React.Component {
 					</div>
 				</section>
 
+				<section class="search-sec">
+					<div class="container">
+						<form action="#" method="post" novalidate="novalidate">
+							<div class="row">
+								<div class="col-lg-12">
+									<div class="row">
+										<div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            				<input type="text" class="form-control search-slt" placeholder="Categoria"/>
+                        				</div>
+										<div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            				<input type="text" class="form-control search-slt" placeholder="Servicio"/>
+                        				</div>
+										<div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            				<select class="form-control search-slt" id="exampleFormControlSelect1">
+												<option>Busqueda</option>
+												<option>Example one</option>
+												<option>Example one</option>
+												<option>Example one</option>
+												<option>Example one</option>
+												<option>Example one</option>
+												<option>Example one</option>
+                            				</select>
+                        				</div>
+										<div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            				<button type="button" class="btn btn-danger wrn-btn">Buscar</button>
+                        				</div>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+				</section>
+
 				<div className="container-fluid mt-4">
 		        	<div className="subcontainer">	
 		        		<Search handleSearch ={this.handleSearch}/>
