f3bd2bd947422af6ce0edbadd7d5ca9b3c56979f
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-07-22 11:31:50 -0500

    Update ListaContainer.js

--- src/Components/ListaHome/ListaContainer.js
+++ src/Components/ListaHome/ListaContainer.js
@@ -59,6 +59,21 @@ class ListaContainer extends React.Component {
 		        	</div>
 	        	</div>
 				<br/>
+
+				<div class="w3-content w3-padding">
+					<div class="w3-container w3-padding-32" id="projects">
+        				<h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Projects</h3>
+            		</div>
+
+					<div class="w3-row-padding">
+						<div class="w3-col l3 m6 w3-margin-bottom">
+                			<div class="w3-display-container">
+            					<div class="w3-display-topleft w3-black w3-padding">Mascotas</div>
+	                				<img src="../Img/mascotas.jpg" alt="House" width="200" height="250"></img>
+                				</div>
+              			</div>
+					</div>
+				</div>
 				<div className="container bg-light">
 					<div className="row justify-content-center"> 
 				        {listas.map((list) => <Lista
