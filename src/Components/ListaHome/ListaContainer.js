31cfd0c16a9a0f24bc2db1683343dd9a53b48e13
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-07-22 16:06:27 -0500

    Update ListaContainer.js

--- src/Components/ListaHome/ListaContainer.js
+++ src/Components/ListaHome/ListaContainer.js
@@ -59,29 +59,38 @@ class ListaContainer extends React.Component {
 		        	</div>
 	        	</div>
 				<br/>
-				<div className="w3-content w3-padding">
-					<div className="w3-row- padding">
-						<div className="w3-col l3 m6 w3-margin-bottom">
-							<div className="w3-display-container">
-								<div className="w3-display-topleft w3-black w3-padding">Albañil</div>
-									<img src="../Img/SVG/labor.svg" alt="Albanil" width="100" height="100"></img>
+				<br/>
+				<br/>
+				<br/>
+				<section class="search-sec">
+					<div class="w3-content w3-padding">
+						<div class="w3-container w3-padding-32" id="projects">
+              				<h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Servicios</h3>
+            			</div>
+						<div className="w3-content w3-padding">
+							<div className="w3-row- padding">
+								<div className="w3-col l3 m6 w3-margin-bottom">
+									<div className="w3-display-container">
+										<div className="w3-display-topleft w3-black w3-padding">Albañil</div>
+											<img src="../Img/SVG/labor.svg" alt="Albanil" width="100" height="100"></img>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="w3-content w3-padding">
+							<div className="w3-row- padding">
+								<div className="w3-col l3 m6 w3-margin-bottom">
+									<div className="w3-display-container">
+										<div className="w3-display-topleft w3-black w3-padding">Arquitectos </div>
+											<img src="../Img/SVG/obrero.svg" alt="Arquitectos " width="100" height="100"></img>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-
-				<div className="w3-content w3-padding">
-					<div className="w3-row- padding">
-						<div className="w3-col l3 m6 w3-margin-bottom">
-							<div className="w3-display-container">
-								<div className="w3-display-topleft w3-black w3-padding">Arquitectos </div>
-									<img src="../Img/SVG/obrero.svg" alt="Arquitectos " width="100" height="100"></img>
-							</div>
-						</div>
-					</div>
-				</div>
-
-
+				</section>
+				
 				<div className="container bg-light">
 					<div className="row justify-content-center"> 
 				        {listas.map((list) => <Lista
