9966727e508c186c1ff45d4946ebe793a7071a82
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-07-23 10:28:34 -0500

    Update Menu.js

--- src/Components/ListaHome/Menu.js
+++ src/Components/ListaHome/Menu.js
@@ -23,13 +23,13 @@ class Menu extends React.Component {
 							<div className="w3-row- padding">
 								<div className="w3-col l3 m6 w3-margin-bottom">
 									<div className="w3-display-container">
-										<div className="w3-display-topleft w3-black w3-padding">Albañil</div>
-											<img src="../Img/SVG/labor.svg" alt="Albanil" width="100" height="200"></img>
+										<div className="w3-display-topleft w3-black w3-padding">Arquitecto</div>
+											<img src="../Img/SVG/obrero.svg" alt="Arquitecto" width="100" height="200"></img>
 									</div>
 								</div>
 							</div>
 						</div>
-                        
+
 					</div>
 				</section>
             </div>
