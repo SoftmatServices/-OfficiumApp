40ee2961f40fbce484b7e61f41ccc87bce1f1ff0
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-07-23 10:08:39 -0500

    Update ListaContainer.js

--- src/Components/ListaHome/ListaContainer.js
+++ src/Components/ListaHome/ListaContainer.js
@@ -87,20 +87,6 @@ class ListaContainer extends React.Component {
 						</div>
 					</div>
 				</section>
-				
-				<div className="container bg-light">
-					<div className="row justify-content-center"> 
-				        {listas.map((list) => <Lista
-				        	//key = {list.string()}
-				        	name = {list.name} 
-				        	category = {list.category}
-							description = {list.description}
-							serviceCategoryCdId = {list.service_category_ad_id}
-							serviceUserId = {list.service_user_id}
-						/>)
-						}
-				</div>	
-			    </div>
 		    </div>
 		)
 	}
