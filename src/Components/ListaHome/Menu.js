71f55d6f199a79e6a7e0f9669f0d151104e7c8d8
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-07-23 15:37:05 -0500

    Update Menu.js

--- src/Components/ListaHome/Menu.js
+++ src/Components/ListaHome/Menu.js
@@ -107,6 +107,49 @@ class Menu extends React.Component {
 							</div>
 						</div>
 
+						<div className="w3-row- padding">
+							<div className="w3-row- padding">
+								<div className="w3-col l3 m6 w3-margin-bottom">
+									<div className="w3-display-container">
+										<div className="w3-display-topleft w3-black w3-padding">Limpieza</div>
+											<img src="../Img/SVG/Limpieza.svg" alt="Limpieza" width="100" height="200"></img>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="w3-row- padding">
+							<div className="w3-row- padding">
+								<div className="w3-col l3 m6 w3-margin-bottom">
+									<div className="w3-display-container">
+										<div className="w3-display-topleft w3-black w3-padding">Pintores</div>
+											<img src="../Img/SVG/Pintores.svg" alt="Pintores" width="100" height="200"></img>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="w3-row- padding">
+							<div className="w3-row- padding">
+								<div className="w3-col l3 m6 w3-margin-bottom">
+									<div className="w3-display-container">
+										<div className="w3-display-topleft w3-black w3-padding">Plomeria</div>
+											<img src="../Img/SVG/Plomeria.svg" alt="Plomeria" width="100" height="200"></img>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="w3-row- padding">
+							<div className="w3-row- padding">
+								<div className="w3-col l3 m6 w3-margin-bottom">
+									<div className="w3-display-container">
+										<div className="w3-display-topleft w3-black w3-padding">Tecnicos</div>
+											<img src="../Img/SVG/Tecnicos.svg" alt="Tecnicos" width="100" height="200"></img>
+									</div>
+								</div>
+							</div>
+						</div>
 
 					</div>
 				</section>
