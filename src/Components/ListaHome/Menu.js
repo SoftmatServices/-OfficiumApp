3bb60848191e7f5316554f0dee671077142b285a
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-07-23 11:20:54 -0500

    Update Menu.js

--- src/Components/ListaHome/Menu.js
+++ src/Components/ListaHome/Menu.js
@@ -41,6 +41,28 @@ class Menu extends React.Component {
 							</div>
 						</div>
 
+						<div className="w3-row- padding">
+							<div className="w3-row- padding">
+								<div className="w3-col l3 m6 w3-margin-bottom">
+									<div className="w3-display-container">
+										<div className="w3-display-topleft w3-black w3-padding">Belleza</div>
+											<img src="../Img/SVG/moda.svg" alt="Belleza" width="100" height="200"></img>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="w3-row- padding">
+							<div className="w3-row- padding">
+								<div className="w3-col l3 m6 w3-margin-bottom">
+									<div className="w3-display-container">
+										<div className="w3-display-topleft w3-black w3-padding">Entrenadores</div>
+											<img src="../Img/SVG/velocidad.svg" alt="Entrenadores" width="100" height="200"></img>
+									</div>
+								</div>
+							</div>
+						</div>
+
 
 					</div>
 				</section>
