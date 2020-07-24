ce9c561f63149c39f389324040ed61df02c7441b
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-07-24 15:10:12 -0500

    Create Albaniles.js

---
+++ src/Components/Servicios/Albaniles.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import { getDeptosAll, getMpios } from '../Services/Get';
+class Albaniles extends Component{
+    render(){
+        return{
+            
+        };
+    }
+}
+export default Albaniles;
