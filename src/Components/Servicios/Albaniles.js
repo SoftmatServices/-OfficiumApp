ffe42050e100f4c010aad583ab6e656b2db5cad6
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-07-30 15:26:55 -0500

    Update Albaniles.js

--- src/Components/Servicios/Albaniles.js
+++ src/Components/Servicios/Albaniles.js
@@ -1,10 +1,17 @@
-import React, {Component} from 'react';
-import { getDeptosAll, getMpios } from '../Services/Get';
+import React, { Component } from 'react';
 class Albaniles extends Component{
     render(){
-        return{
-            
-        };
+        return(
+            <div className="col-sm-13 ">
+                <div className="col-sm-12" id="fixed-sidebar">
+                    
+                </div>
+                <ul>
+                    <img src="../Img/SVG/obrero.svg" alt="Arquitectos" width="100" height="200" className="fuente-fjalla ul-personalizada"></img>
+                    <img src="../Img/SVG/coche.svg" alt="Automóviles" width="100" height="200"></img>
+                </ul>
+            </div>
+        );
     }
 }
 export default Albaniles;
