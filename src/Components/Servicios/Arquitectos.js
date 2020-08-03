b989b77ffcbe8f586a648dfdd81358f8b7da0e9b
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-08-03 15:52:18 -0500

    Create Arquitectos.js

---
+++ src/Components/Servicios/Arquitectos.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import '../../css/Latera.css';
+import '../../css/Menu.css';
+import ListaContainer from '../ListaHome/ListaContainer';
+class Arquitectos extends Component {
+    render() {
+        return (
+            <div>
+                <div class="wrapper">
+                    <nav id="sidebar">
+                        <div class="sidebar-header">
+                            <h3>Arquitectos</h3>
+                        </div>
+                        <ul class="list-unstyled components">
+                            <p>Bienvenidos</p>
+                            <li class="active">
+                                <a href="#menu">Albañiles</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Automóviles</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Belleza</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Carpinteros</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Cerrajeros</a>
+                            </li>
+                            <li class="active">
+                                <a href="#menu">Entrenadores</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Electricistas</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Reparaciones</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Limpieza</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Pintores</a>
+                            </li>
+                            <li class="active">
+                                <a href="#menu">Plomeria</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Tecnicos</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div>
+                    <ListaContainer />
+                </div>
+            </div>
+        );
+    }
+}
+export default Arquitectos;
