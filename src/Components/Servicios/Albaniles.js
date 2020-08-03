8474028666dc78d5d416e6b080230f3ad87d7b3a
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-08-03 10:13:17 -0500

    Update Albaniles.js

--- src/Components/Servicios/Albaniles.js
+++ src/Components/Servicios/Albaniles.js
@@ -4,7 +4,31 @@ class Albaniles extends Component {
     render() {
         return (
             <div>
-                
+                <div class="wrapper">
+                    <nav id="sidebar">
+                        <div class="sidebar-header">
+                            <h3>Albañiles</h3>
+                        </div>
+                        <ul class="list-unstyled components">
+                            <p>Dummy Heading</p>
+                            <li class="active">
+                                <a href="#menu">Animación</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Ilustración</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Blog</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Acerca</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Contacto</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         );
     }
