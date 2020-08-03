20219f6757b0b38574b9df5b5122eb4c2547ac68
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-08-03 10:27:26 -0500

    Update ListaContainer.js

--- src/Components/ListaHome/ListaContainer.js
+++ src/Components/ListaHome/ListaContainer.js
@@ -35,24 +35,6 @@ class ListaContainer extends React.Component {
 
 		return (
 			<div >
-				<section>
-					<div id="carouselExampleFade" className="carousel slide carousel-fade" data-ride="carousel">
-						<div className="carousel-inner">
-							<div className="carousel-item active">
-								<div className="carousel-item active">
-									<img src="../Img/imagen.jpg" class="d-block w-100" alt="" width="1400" height="700"></img>
-								</div>
-								<div class="carousel-item">
-                					<img src="../Img/imagen.jpg" class="d-block w-100" alt="..." ></img>
-            					</div>
-								<div class="carousel-item">
-               				  	   <img src="../Img/imagen.jpg" class="d-block w-100" alt="..."></img>
-           						</div>
-							</div>
-						</div>
-					</div>
-				</section>
-
 				<div className="container-fluid mt-4">
 		        	<div className="subcontainer">	
 		        		<Search handleSearch ={this.handleSearch}/>
@@ -76,4 +58,4 @@ class ListaContainer extends React.Component {
 	}
 }
 
-export default ListaContainer
+export default ListaContainer;
