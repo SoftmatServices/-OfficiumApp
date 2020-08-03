f9467d677c16a9e9992d21fc5c11de9001e4a591
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-08-03 15:20:58 -0500

    Update Search.js

--- src/Components/ListaHome/Search.js
+++ src/Components/ListaHome/Search.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import '../../css/Menu.css';
+import '../../css/Menus.css';
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import PropTypes from 'prop-types';
 
@@ -19,24 +19,35 @@ class Search extends React.Component{
         const {handleSearch} = this.props
         const {search} = this.state
         return(
-            
-            <div className="row justify-content-center" >
-                <div className="col-lg-3 col-md-3 col-sm-12 p-0">
-                    <input className="form-control search-slt" placeholder="Busqueda"
-                            value={search}
-                            type="text" 
-                            onChange={this.handleChange}
-                            />
-                </div>
-                <div className="col-lg-3 col-md-3 col-sm-12 p-0">
-                    <input type="text" class="form-control search-slt" placeholder="Servicio"/>
-                </div>
-                <div className="col-md-2 col-sm-6">
-                        <button type="button" 
-                            className="btn btn-danger wrn-btn" 
-                            onClick={() => handleSearch(search)}>Buscar
-                        </button>
-                </div>
+            <div>
+                <section className="search-sec">
+                    <div>
+                        <form action="#" method="post" novalidate="novalidate">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="row">
+                                        <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                                            <input className="form-control search-slt" placeholder="Busqueda"
+                                            value={search}
+                                            type="text" 
+                                            onChange={this.handleChange}
+                                            />
+                                        </div>
+                                        <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                                            <input type="text" className="form-control search-slt" placeholder="Servicio"/>
+                                        </div>
+                                        <div className="col-md-2 col-sm-6">
+                                            <button type="button" 
+                                                className="btn btn-danger wrn-btn" 
+                                                onClick={() => handleSearch(search)}>Buscar
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </section>
             </div>
         );
     }
