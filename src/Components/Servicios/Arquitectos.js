48dd0e1cb4c4977a5f8b4b0870958ce198ead9d1
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-08-04 10:52:46 -0500

    Update Arquitectos.js

--- src/Components/Servicios/Arquitectos.js
+++ src/Components/Servicios/Arquitectos.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../../css/Latera.css';
-import '../../css/Menu.css';
+import '../../css/Menus.css';
 import ListaContainer from '../ListaHome/ListaContainer';
 class Arquitectos extends Component {
     render() {
