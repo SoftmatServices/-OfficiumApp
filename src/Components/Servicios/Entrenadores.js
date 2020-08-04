a5b99fda678d8ab6f65dc403afbf14f4fd5ce318
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-08-04 16:19:22 -0500

    Update Entrenadores.js

--- src/Components/Servicios/Entrenadores.js
+++ src/Components/Servicios/Entrenadores.js
@@ -8,7 +8,7 @@ import {
     Route,
     Link
 } from "react-router-dom";
-class Arquitectos extends Component {
+class Entrenadores extends Component {
     render() {
         return (
             <div>
@@ -65,4 +65,4 @@ class Arquitectos extends Component {
         );
     }
 }
-export default Arquitectos;
+export default Entrenadores;
