05e30f4436446460b0b12be8a2f1797b79290edc
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-08-04 15:20:34 -0500

    Create Automoviles.js

---
+++ src/Components/Servicios/Automoviles.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import '../../css/Latera.css';
+import '../../css/Menus.css';
+import ListaContainer from '../ListaHome/ListaContainer';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+
+class Albaniles extends Component {
+    render() {
+        return (
+            <div>
+                <div className="wrapper">
+                    <nav id="sidebar">
+                        <div className="sidebar-header">
+                            <h3>Automóviles</h3>
+                        </div>
+                        <ul className="list-unstyled components">
+                            <p>Bienvenidos</p>
+                            <li className="active">
+                                <Link to="/Arquitectos">Albañiles</Link>
+                            </li>
+                            <li>
+                                <a href="#menu">Arquitectos</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Belleza</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Carpinteros</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Cerrajeros</a>
+                            </li>
+                            <li className="active">
+                                <a href="#menu">Entrenadores</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Electricistas</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Reparaciones</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Limpieza</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Pintores</a>
+                            </li>
+                            <li className="active">
+                                <a href="#menu">Plomeria</a>
+                            </li>
+                            <li>
+                                <a href="#menu">Tecnicos</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div>
+                    <ListaContainer />
+                </div>
+            </div>
+        );
+    }
+}
+export default Albaniles;
