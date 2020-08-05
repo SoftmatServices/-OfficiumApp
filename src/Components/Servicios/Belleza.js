20a78c991653a2b74cb0511dc3b251a8be87d0ae
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-08-05 10:00:32 -0500

    Update Belleza.js

--- src/Components/Servicios/Belleza.js
+++ src/Components/Servicios/Belleza.js
@@ -17,7 +17,7 @@ class Belleza extends Component {
                 <div className="wrapper">
                     <nav id="sidebar">
                         <div className="sidebar-header">
-                            <h3>Albañiles</h3>
+                            <h3>Belleza</h3>
                         </div>
                         <ul className="list-unstyled components">
                             <p><Link to="/Menus">Menu</Link></p>
