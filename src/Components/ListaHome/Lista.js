183d1d63d87350bd3aee8ec3ea97e75674222a4c
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-08-06 10:10:39 -0500

    Update Lista.js

--- src/Components/ListaHome/Lista.js
+++ src/Components/ListaHome/Lista.js
@@ -3,17 +3,13 @@ import '../../css/List.css';
 import PropTypes from 'prop-types'
 
 const Lista = ({name, category, description, serviceCategoryCdId, serviceUserId}) => (
-	              
         <div className="col-6 col-xs-12 col-sm-6 col-md-4 col-lg-3 mt-2 bg-white">
-             <div className="title">
+            <div className="title">
                 <p>Nombre: {name}</p>
                 <p>Categoria: {category}</p>
                 <p>Descripción: {description}</p>
-             </div>             
+            </div>             
         </div>
-    
-     
-      
     )
 
 Lista.propTypes = {
