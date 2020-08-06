150315b946391ceab520264db1eef0a61e6db758
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-08-06 10:14:07 -0500

    Update Menus.js

--- src/Components/Servicios/Menus.js
+++ src/Components/Servicios/Menus.js
@@ -5,28 +5,17 @@ class Menus extends React.Component {
 	render() {
 		return (
 			<div>
-				<section class="search-sec">
-					<div class="">
+				<section className="search-sec">
+					<div className="">
 						<form action="#" method="post" novalidate="novalidate">
-							<div class="row">
-								<div class="col-lg-12">
-									<div class="row">
-										<div class="col-lg-3 col-md-3 col-sm-12 p-0">
-											<input type="text" class="form-control search-slt" placeholder="Servicio" />
+							<div className="row">
+								<div className="col-lg-12">
+									<div className="row">
+										<div className="col-lg-50 col-md-50 col-sm-8">
+											<input type="text" className="form-control search-slt" placeholder="Servicio" />
 										</div>
-										<div class="col-lg-3 col-md-3 col-sm-12 p-0">
-											<select class="form-control search-slt" id="exampleFormControlSelect1">
-												<option>Busqueda</option>
-												<option>Example one</option>
-												<option>Example one</option>
-												<option>Example one</option>
-												<option>Example one</option>
-												<option>Example one</option>
-												<option>Example one</option>
-											</select>
-										</div>
-										<div class="col-lg-3 col-md-3 col-sm-12 p-0">
-											<button type="button" class="btn btn-danger wrn-btn">Buscar</button>
+										<div className="col-lg-3 col-md-3 col-sm-12 p-0">
+											<button type="button" className="btn btn-danger wrn-btn">Buscar</button>
 										</div>
 									</div>
 								</div>
