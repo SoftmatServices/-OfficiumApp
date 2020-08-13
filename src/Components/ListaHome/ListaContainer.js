9a454aded67efb4cc64a7d49a64fae620bd62375
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-08-13 10:48:57 -0500

    Update ListaContainer.js

--- src/Components/ListaHome/ListaContainer.js
+++ src/Components/ListaHome/ListaContainer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Lista from './Lista';
-import '../../css/List.css';
-import '../../css/Item.css';
+//import '../../css/List.css';
+//import '../../css/Item.css';
+import '../../css/Datos.css';
 import Search from './Search';
 import {getListasByPopular, getListasBySearch} from '../../Services/Get'
 
@@ -40,6 +41,18 @@ class ListaContainer extends React.Component {
 		        		<Search handleSearch ={this.handleSearch}/>
 		        	</div>
 	        	</div>
+				<section className="clientes contenedor">
+					<h2 className="titulo">Que dicen nuestros clientes</h2>
+					<div className="cards">
+						<img src="../Img/SVG/labor.svg" alt=""/>
+						<div className="contenido-texto-card">
+							<h4>Name:</h4>
+                        	<h4>Tel:</h4>
+                        	<h4>Descripción:</h4>
+                        	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, sapiente!</p>
+						</div>
+					</div>
+				</section>
 				<div className="container bg-white non-border">
 					<div className="row justify-content-center"> 
 				        {listas.map((list) => <Lista
