77415c2a7506610b16098e2940f1943b74c002c3
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-08-14 16:14:35 -0500

    Update Lista.js

--- src/Components/ListaHome/Lista.js
+++ src/Components/ListaHome/Lista.js
@@ -10,10 +10,10 @@ const Lista = ({name, category, description, serviceCategoryCdId, serviceUserId}
                         <div className="card">
                             <img src="../Img/SVG/labor.svg" alt=""></img>
                         <div className="contenido-texto-card">
-                            <p>{name}</p>
-                            <p>{category}</p>
-                            <p>{description}</p>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, sapiente!</p>
+                            <p1>{name}</p1>
+                            <p1>{category}</p1>
+                            <p1>{description}</p1>
+                            <p1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, sapiente!</p1>
                         </div>
                     </div>
                 </div>
