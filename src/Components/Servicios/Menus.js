9cf6d02ec0b1aac9b22fe320ea4c972e367686d9
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-09-11 11:34:24 -0500

    Update Menus.js

--- src/Components/Servicios/Menus.js
+++ src/Components/Servicios/Menus.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../css/Letter.css';
-import '../../css/Estilos.css';
+//import '../../css/Estilos.css';
 import Servicio from './Servicio';
 class Menus extends React.Component {
     render() {
@@ -13,7 +13,7 @@ class Menus extends React.Component {
                                 <div className="container-fluid">
                                     <div className="header-mobile-inner">
                                         <a className="logo" href="index.html">
-                                            <img src="/img/Logo.ico" alt="Oficium" />
+                                            <img src="/img/Logo.ico" alt="Oficium"></img>
                                         </a>
                                         <button className="hamburger hamburger--slider" type="button">
                                             <span className="hamburger-box">
@@ -28,7 +28,8 @@ class Menus extends React.Component {
                                     <ul className="navbar-mobile__list list-unstyled">
                                         <li className="has-sub">
                                             <a className="js-arrow" href="index.html">
-                                                <i className="fas fa-tachometer-alt"></i>Datos</a>
+                                                <i className="fas fa-tachometer-alt"></i>Datos
+                                            </a>
                                             <ul className="navbar-mobile-sub__list list-unstyled js-sub-list">
                                                 <li>
                                                     <a href="index.html">Actualización</a>
@@ -40,7 +41,8 @@ class Menus extends React.Component {
                                         </li>
                                         <li className="has-sub">
                                             <a className="js-arrow" href="index.html">
-                                                <i className="fas fa-copy"></i>Registro</a>
+                                                <i className="fas fa-copy"></i>Registro
+                                            </a>
                                             <ul className="navbar-mobile-sub__list list-unstyled js-sub-list">
                                                 <li>
                                                     <a href="login.html">Login</a>
@@ -57,19 +59,20 @@ class Menus extends React.Component {
                                 </div>
                             </nav>
                         </header>
-                        <aside class="menu-sidebar d-none d-lg-block">
-                            <div class="logo">
+                        <aside className="menu-sidebar d-none d-lg-block">
+                            <div className="logo">
                                 <a href="index.html">
                                     <img src="/img/Logo.ico" alt="Oficium" />
                                 </a>
                             </div>
-                            <div class="menu-sidebar__content js-scrollbar1">
-                                <nav class="navbar-sidebar">
-                                    <ul class="list-unstyled navbar__list">
-                                        <li class="active has-sub">
-                                            <a class="js-arrow" href="index.html">
-                                                <i class="fas fa-tachometer-alt"></i>Datos</a>
-                                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                            <div className="menu-sidebar__content js-scrollbar1">
+                                <nav className="navbar-sidebar">
+                                    <ul className="list-unstyled navbar__list">
+                                        <li className="active has-sub">
+                                            <a className="js-arrow" href="index.html">
+                                                <i className="fas fa-tachometer-alt"></i>Datos
+                                        </a>
+                                            <ul className="list-unstyled navbar__sub-list js-sub-list">
                                                 <li>
                                                     <a href="index.html">Actualización</a>
                                                 </li>
@@ -78,50 +81,52 @@ class Menus extends React.Component {
                                                 </li>
                                             </ul>
                                         </li>
-                                        <li class="has-sub">
-                                            <a class="js-arrow" href="index.html">
-                                                <i class="fas fa-copy"></i>Registro</a>
-                                            <ul class="list-unstyled navbar__sub-list js-sub-list">
-                                                <li>
-                                                    <a href="login.html">Login</a>
-                                                </li>
-                                                <li>
-                                                    <a href="register.html">Register</a>
-                                                </li>
-                                                <li>
-                                                    <a href="forget-pass.html">Forget Password</a>
-                                                </li>
-                                            </ul>
+                                        <li className="has-sub">
+                                            <a className="js-arrow" href="index.html">
+                                                <i className="fas fa-copy"></i>Registro
+                                        </a>
                                         </li>
+                                        <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                            <li>
+                                                <a href="login.html">Login</a>
+                                            </li>
+                                            <li>
+                                                <a href="register.html">Register</a>
+                                            </li>
+                                            <li>
+                                                <a href="forget-pass.html">Forget Password</a>
+                                            </li>
+                                        </ul>
                                     </ul>
                                 </nav>
                             </div>
                         </aside>
-                        <div class="page-container">
-                            <header class="header-desktop">
-                                <div class="section__content section__content--p30">
-                                    <div class="container-fluid">
-                                        <div class="header-wrap">
-                                            <form class="form-header" action="" method="POST">
-                                                <input class="au-input au-input--xl" type="text" name="search" placeholder="Search for datas &amp; reports..." />
-                                                <button class="au-btn--submit" type="submit">
-                                                    <i class="zmdi zmdi-search"></i>
+                        <div className="page-container">
+                            <header className="header-desktop">
+                                <div className="section__content section__content--p30">
+                                    <div className="container-fluid">
+                                        <div className="header-wrap">
+                                            <form className="form-header" action="" method="POST">
+                                                <input className="au-input au-input--xl" type="text" name="search" placeholder="Search for datas &amp; reports...">
+
+                                                </input>
+                                                <button className="au-btn--submit" type="submit">
+                                                    <i className="zmdi zmdi-search"></i>
                                                 </button>
                                             </form>
                                         </div>
                                     </div>
                                 </div>
                             </header>
-                            <div class="main-content">
-                                <div class="section__content section__content--p30">
-                                    <div class="container-fluid">
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="overview-wrap">
-                                                    <h2 class="title-1">Servicios</h2>
-
-                                                    <button class="au-btn au-btn-icon au-btn--blue">
-                                                        <i class="zmdi zmdi-plus"></i>Creación Servicios
+                            <div className="main-content">
+                                <div className="section__content section__content--p30">
+                                    <div className="container-fluid">
+                                        <div className="row">
+                                            <div className="col-md-12">
+                                                <div className="overview-wrap">
+                                                    <h2 className="title-1">Servicios</h2>
+                                                    <button className="au-btn au-btn-icon au-btn--blue">
+                                                        <i className="zmdi zmdi-plus"></i>Creación Servicios
                                                     </button>
                                                 </div>
                                                 <div>
@@ -133,30 +138,10 @@ class Menus extends React.Component {
                                 </div>
                             </div>
                         </div>
-                        
                     </div>
-                    
-                        <script src="vendor/jquery-3.2.1.min.js"></script>
-                        <script src="vendor/bootstrap-4.1/popper.min.js"></script>
-                        <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
-                        <script src="vendor/slick/slick.min.js">
-                        </script>
-                        <script src="vendor/wow/wow.min.js"></script>
-                        <script src="vendor/animsition/animsition.min.js"></script>
-                        <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
-                        </script>
-                        <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
-                        <script src="vendor/counter-up/jquery.counterup.min.js">
-                        </script>
-                        <script src="vendor/circle-progress/circle-progress.min.js"></script>
-                        <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
-                        <script src="vendor/chartjs/Chart.bundle.min.js"></script>
-                        <script src="vendor/select2/select2.min.js"></script>
-                        <script src="js/main.js"></script>
                 </body>
             </div>
         );
-    }
+    };
 }
-
 export default Menus;
