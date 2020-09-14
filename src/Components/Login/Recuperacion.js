74f1780d3c9061fe45598c5c309e083ccb8b5e69
AplicacionLogin <66234080+AplicacionLogin@users.noreply.github.com>
2020-09-14 15:30:22 -0500

    Update Recuperacion.js

--- src/Components/Login/Recuperacion.js
+++ src/Components/Login/Recuperacion.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../css/Estilos.css';
+import '../../css/Principal/Estilos.css';
 class Recuperacion extends React.Component {
 
 render(){
@@ -20,9 +20,9 @@ render(){
                     <form action="" method="post">
                       <div className="form-group">
                         <label>Email Address</label>
-                        <input class="au-input au-input--full" type="email" name="email" placeholder="Email"></input>
+                        <input className="au-input au-input--full" type="email" name="email" placeholder="Email"></input>
                       </div>
-                      <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">submit</button>
+                      <button className="au-btn au-btn--block au-btn--green m-b-20" type="submit">submit</button>
                     </form>
                   </div>
                 </div>
